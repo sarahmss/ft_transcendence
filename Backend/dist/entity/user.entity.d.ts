00a4ffbf753d03f85e23114835db7da0107c3d62
smodesto <smodesto@student.42sp.org.br>
2023-10-18 23:13:08 -0300

    notFound, try catch block + notUnique

---
+++ Backend/dist/entity/user.entity.d.ts
@@ -0,0 +1,7 @@
+export declare class User {
+    userId: string;
+    userName: string;
+    email: string;
+    password: string;
+    profilePicture: string;
+}
---
+++ Backend/dist/users/dto/user.dto.d.ts
@@ -0,0 +1,6 @@
+export declare class CreateUserDto {
+    userName: string;
+    email: string;
+    password: string;
+    passwordConfirm: string;
+}
