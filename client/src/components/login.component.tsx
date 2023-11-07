c5c12e10d057fb4da3a48c8cfbef07e56182b793
smodesto <smodesto@student.42sp.org.br>
2023-11-06 22:12:51 -0300

    profile

---
+++ client/src/components/login.component.tsx
@@ -0,0 +1,145 @@
+import { Component } from "react";
+import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import axios from "axios";
+
+import customIcon from '../assets/42logo.svg';
+
+const loginLink = process.env.REACT_APP_BACK_HOST + "/auth/login"
+
+
+type Props = {};
+
+type State = {
+	redirect: string | null,
+	username: string,
+	password: string,
+	loading: boolean,
+	message: string
+};
+
+
+export default class Login extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.handleLogin = this.handleLogin.bind(this);
+
+		this.state = {
+			redirect: null,
+			username: "",
+			password: "",
+			loading: false,
+			message: ""
+		};
+	}
+
+	// componentDidMount() {
+	// 	const currentUser = AuthService.getCurrentUser();
+
+	// 	if (currentUser) {
+	// 		this.setState({ redirect: "/profile" });
+	// 	};
+	// }
+
+	componentWillUnmount() {
+		window.location.reload();
+	}
+
+	handleLogin = async (formValues: { username: string; password: string }) => {
+		const { username, password } = formValues;
+		try {
+		  const response = await axios.post(loginLink, {
+			username,
+			password,
+		  });
+
+		} catch (error) {
+
+		console.error("Erro ao fazer login:", error);
+		}
+	  };
+
+	render() {
+		if (this.state.redirect) {
+			return <Navigate to={this.state.redirect} />
+		}
+
+		const { loading, message } = this.state;
+
+		const initialValues = {
+			username: "",
+			password: "",
+		};
+
+		return (
+			<div className="col-md-12">
+				<div className="card card-container">
+					<img
+						src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+						alt="profile-img"
+						className="profile-img-card"
+					/>
+
+					<Formik
+						initialValues={initialValues}
+						onSubmit={this.handleLogin}
+					>
+						<Form>
+							<div className="form-group">
+								<label htmlFor="username">Username</label>
+								<Field name="username" type="text" className="form-control" />
+								<ErrorMessage
+									name="username"
+									component="div"
+									className="alert alert-danger"
+								/>
+							</div>
+
+							<div className="form-group">
+								<label htmlFor="password">Password</label>
+								<Field name="password" type="password" className="form-control" />
+								<ErrorMessage
+									name="password"
+									component="div"
+									className="alert alert-danger"
+								/>
+							</div>
+
+							<div className="form-group">
+								<button type="submit" className="btn btn-primary btn-block" disabled={loading}>
+									{loading && (
+										<span className="spinner-border spinner-border-sm"></span>
+									)}
+									<span>Login</span>
+								</button>
+							</div>
+
+							<div className="form-group">
+								<Link to={loginLink}>
+									<button
+										type="submit"
+										className="btn btn-primary btn-block">
+										<span>
+										<img src={customIcon} height="24"
+										width="24" alt="Icon" />
+											Login with 42
+										</span>
+									</button>
+								</Link>
+							</div>
+
+							{message && (
+								<div className="form-group">
+									<div className="alert alert-danger" role="alert">
+										{message}
+									</div>
+								</div>
+							)}
+						</Form>
+					</Formik>
+				</div>
+			</div>
+		);
+	}
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+reportWebVitals();
