7a2087b6745e071614bf11fb43f8c9f5445367a5
Leticia Franca <canumsang@gmail.com>
2024-02-02 17:50:22 -0300

    adds MatchHistory file

---
+++ server/src/entity/match.entity.ts
@@ -0,0 +1,33 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+
+import { User } from './user.entity';
+
+@Entity()
+export class Match {
+
+    @PrimaryGeneratedColumn("uuid")
+    gameId: string;
+
+    @ManyToOne(() => User, { nullable: false })
+    @JoinColumn({ name: "winnerId" })
+    winner: User;
+
+    @ManyToOne(() => User, { nullable: false })
+    @JoinColumn({ name: "loserId" })
+    loser: User;
+
+    @Column({ nullable: false })
+    winnerScore: number;
+
+    @Column({ nullable: false })
+    loserScore: number;
+
+    @Column({ nullable: false })
+    gameTime: number;
+}
