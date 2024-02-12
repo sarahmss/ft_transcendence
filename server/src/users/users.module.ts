42cea17a9a270f8a361387a032c5c928e499461a
root <root@Sarah-PC>
2024-02-12 01:01:49 -0300

    trying to solve changes

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,10 +2,14 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
+import { Friends } from '../entity/friends.entity';
 import { Module, Logger } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Friends]),
+  ],
   controllers: [UsersController],
   providers: [UsersService, Logger],
   exports: [UsersService, TypeOrmModule]
