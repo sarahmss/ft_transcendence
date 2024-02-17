f7cec7647041fbab9f9c874a0cbd2cf86df6ce69
root <root@Sarah-PC>
2024-02-17 19:46:48 -0300

    uploadService

--- server/src/uploads/upload.service.ts
+++ server/src/uploads/upload.service.ts
@@ -6,17 +6,6 @@ const fs = require('fs');
 export class UploadsService {
   constructor() {}
 
-
-  // async saveProfilePicture(userId: string, file: Express.Multer.File): Promise<string> {
-  //   const profilePicturesPath = join(process.cwd(), `/uploads/${userId}/profilePictures`);
-  //   if (!fs.existsSync(profilePicturesPath)) {
-  //     fs.mkdirSync(profilePicturesPath, { recursive: true });
-  //   }
-
-  //   const url = `/uploads/${userId}/profilePictures/${file.filename}`;
-  //   return url;
-  // }
-
   async getFile(userId: string): Promise<string> {
     const qrCodeLink = `/uploads/${userId}/qrcode.png`;
     const filePath = join(process.cwd(), qrCodeLink);
@@ -27,7 +16,7 @@ export class UploadsService {
       throw new NotFoundException();
     }
   }
-  
+
   async getProfilePicture(userId: string, filename: string): Promise<string> {
 		const filePath = join(process.cwd(), `uploads/${userId}/profilePictures/${filename}`);
 
