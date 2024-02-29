afc8e4ef07e84f91e1b03744572bbc2e5a2cffc0
joseisp <silvapeixotojoseinacio@gmail.com>
2024-02-28 21:52:18 -0300

    await fixed

--- server/src/game/game.gateway.ts
+++ server/src/game/game.gateway.ts
@@ -131,8 +131,8 @@ export class GameGateway
 		}
 
 	@SubscribeMessage('leaveRoom')
-		handleLeaveRoom(client: Socket) : void {
-			this.gameService.leaveRoomInit(client, this.server);
+		async handleLeaveRoom(client: Socket) : Promise<void> {
+			await this.gameService.leaveRoomInit(client, this.server);
 		}
 
 	@SubscribeMessage('gameLoaded')
